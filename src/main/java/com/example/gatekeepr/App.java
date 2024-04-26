718c2650252ccaad10ceb1a8e57e7d6996129f52
eduard <eduard.gjuraj@student.upt.ro>
2024-04-27 02:33:15 +0300

    added database testing

--- src/main/java/com/example/gatekeepr/App.java
+++ src/main/java/com/example/gatekeepr/App.java
@@ -1,12 +1,15 @@
 package com.example.gatekeepr;
 
+import com.example.gatekeepr.Classes.Admin;
 import com.example.gatekeepr.Controllers.SchemaInitializer;
+import com.example.gatekeepr.Database.DBOperationsAdmins;
 import com.example.gatekeepr.Models.Model;
-import com.example.gatekeepr.Views.ViewFactory;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 public class App extends Application {
 
@@ -14,5 +17,6 @@ public class App extends Application {
     public void start(Stage stage) {
         SchemaInitializer.initializeDatabase();
         Model.getInstance().getViewFactory().showLoginWindow();
+
     }
 }
